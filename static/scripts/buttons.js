24b41d8207f15e58e64fa14329463d789c1d19dd
Isaac Samuel <getintouchwithisaac@gmail.com>
2017-01-12 17:08:20 -0500

    completed button logic (skeleton)

--- static/scripts/buttons.js
+++ static/scripts/buttons.js
@@ -9,3 +9,19 @@ function log_in(logged_in) {
 		window.location = "/log_in"
 	}
 }
+
+function show_high_scores() {
+	window.location.href='high_scores'
+}
+
+function new_game(logged_in) {
+	if (logged_in) {
+		confirmation = confirm("Are you sure you want to make a new game? Your current game and your progress will be lost.")
+			if(confirmation) {
+				window.location = "/new_game"
+			}
+	}
+	else {
+		alert("To start a new game, you must first log in. Press the Log in / Log out button.")
+	}
+}
