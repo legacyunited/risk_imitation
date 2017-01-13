266a17743922e5761df8cabf234249c5ca8fe22e
Isaac Samuel <getintouchwithisaac@gmail.com>
2017-01-12 19:51:47 -0500

    added new_game.html; started on routing logic

--- main.py
+++ main.py
@@ -1,10 +1,29 @@
 from flask import Flask, request, render_template
 from routing import routing
 
-
 app = Flask(__name__)
 app.debug = True
 
+
 @app.route('/')
 def home():
-	return render_template("index.html")
+	logged_in = False
+
+	if not logged_in:
+		blank_game = Game()
+		return render_template("index.html")
+	else:
+		return render_template("index.html", Game(User.name))
+
+
+@app.route('/new_game/', methods=["POST", "GET"])
+def new_game():
+	if request.method == "GET":
+		return render_template("new_game.html")
+
+	if request.method == "POST":
+		if new_game_input_is_good(request.form):
+			return home()
+		else:
+			return render_template("new_game.html", request.form)
+
--- static/scripts/buttons.js
+++ static/scripts/buttons.js
@@ -2,11 +2,11 @@ function log_in(logged_in) {
 	if (logged_in) {
 		log_out = confirm("Are you sure you want to log out? Your current game will be saved.")
 			if(log_out) {
-				window.location = "/log_out"
+				window.location = "/log_out/"
 			}
 	}
 	else {
-		window.location = "/log_in"
+		window.location = "/log_in/"
 	}
 }
 
@@ -18,10 +18,14 @@ function new_game(logged_in) {
 	if (logged_in) {
 		confirmation = confirm("Are you sure you want to make a new game? Your current game and your progress will be lost.")
 			if(confirmation) {
-				window.location = "/new_game"
+				window.location = "/new_game/"
 			}
 	}
 	else {
 		alert("To start a new game, you must first log in. Press the Log in / Log out button.")
 	}
+}
+
+function go_back() {
+    window.history.back();
 }
