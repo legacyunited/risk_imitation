0ae514013625a264ef2cd3544c2b2b0d2f0b64ca
Isaac Samuel <getintouchwithisaac@gmail.com>
2017-01-15 11:04:01 -0500

    Country class; displaying troop numbers on index page

--- main.py
+++ main.py
@@ -1,17 +1,27 @@
 from flask import Flask, request, render_template
-from routing import routing
 
 app = Flask(__name__)
 app.debug = True
+
+class Country:
+	def __init__(self, country):
+		self.country_name = country
+		self.troops = 0
 
 
 @app.route('/')
 def home():
 	logged_in = False
-
+	
 	if not logged_in:
-		blank_game = Game()
-		return render_template("index.html")
+		result = []
+		with open("countries.txt", "r") as file:
+			for line in file:
+				country = line.rstrip()
+				result.append(Country(country))
+
+		return render_template("index.html", countries=result)
+
 	else:
 		return render_template("index.html", Game(User.name))
 
