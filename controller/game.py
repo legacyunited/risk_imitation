b796917c529896b5c30419b7433d39b0ac134676
Isaac Samuel <getintouchwithisaac@gmail.com>
2017-01-25 18:16:32 -0500

    completed game.give_risk_troops

--- controller/game.py
+++ controller/game.py
@@ -8,3 +8,17 @@ class Game:
 
 	def roll_die()
 		return Random.randint(1, 6)
+
+	def give_risk_troops();
+		if self.risk_cards_claimed == 0:
+			self.risk_cards_claimed += 1
+			return 4
+		if self.risk_cards_claimed == 1:
+			self.risk_cards_claimed += 1
+			return 6
+		if self.risk_cards_claimed == 2:
+			self.risk_cards_claimed += 1
+			return 8
+		if self.risk_cards_claimed > 2:
+			self.risk_cards_claimed += 1
+			return (self.risk_cards_claimed-1) * 5
